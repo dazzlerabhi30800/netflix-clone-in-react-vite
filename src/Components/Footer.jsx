32c4b3a1b9ea334ce14dd4309ddea0c6dca89586
= <abhisingh30800@gmail.com>
2023-01-23 18:30:33 +0530

    fixed footer text align

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer>
       <img src="./img/netflix-logo.png" alt="Netflix" />
-      <h2>&copy; 2023 NetflixClone All right reserved </h2>
+      <h2>&copy; 2023 NetflixClone All rights reserved </h2>
       <span className="note">
         This website Clone is only for learning purposes and not a official
         website.
